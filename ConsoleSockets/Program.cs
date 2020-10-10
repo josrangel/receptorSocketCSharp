836e36c56f90bd3eeeccb243eb7d9144ce43a313
Josue <josrangel@hotmail.com>
2020-10-10 17:43:21 -0500

    primer commit

---
+++ ConsoleSockets/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;               //   Paso 1
+using System.Net.Sockets;       //   Paso 1
+
+namespace ConsoleSockets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Conectar();
+        }
+
+        public static void Conectar()
+        {
+            byte[] ByRec;
+            Socket miPrimerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint miDireccion = new IPEndPoint(IPAddress.Any, 1234);
+
+            try
+            {
+                miPrimerSocket.Bind(miDireccion);
+                miPrimerSocket.Listen(1);
+
+                Console.WriteLine("Escuchando...");
+                while (true)
+                {
+                    Socket Escuchar = miPrimerSocket.Accept();
+                    Console.WriteLine("Conectado con exito");
+
+                    ByRec = new byte[255];
+                    int a = Escuchar.Receive(ByRec, 0, ByRec.Length, 0);
+                    Array.Resize(ref ByRec, a);
+                    Console.WriteLine("Cliente dice: " + Encoding.Default.GetString(ByRec)); //mostramos lo recibido
+                }
+
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine("Error: {0}", error.ToString());
+            }
+            finally
+            {
+                miPrimerSocket.Close();
+            }
+            Console.WriteLine("Presione cualquier tecla para terminar");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleSockets/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ConsoleSockets")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConsoleSockets")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6a60af2c-180b-47e5-90b2-2bd82d3f1df3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
